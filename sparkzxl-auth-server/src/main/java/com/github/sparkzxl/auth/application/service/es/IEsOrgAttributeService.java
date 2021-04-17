8352e685e1458f1ede738230a1c583d0787e7591
zhouxinlei <zhouxinlei298@163.com>
2021-04-17 21:15:49 +0800

    代码优化

---
+++ sparkzxl-auth-server/src/main/java/com/github/sparkzxl/auth/application/service/es/IEsOrgAttributeService.java
@@ -0,0 +1,13 @@
+package com.github.sparkzxl.auth.application.service.es;
+
+import com.github.sparkzxl.elasticsearch.service.base.IBaseElasticsearchService;
+
+/**
+ * description: ES组织属性 服务类
+ *
+ * @author charles.zhou
+ * @date 2021-03-24 15:52:17
+ */
+public interface IEsOrgAttributeService extends IBaseElasticsearchService {
+
+}
---
+++ sparkzxl-auth-server/src/main/java/com/github/sparkzxl/auth/domain/service/es/EsOrgAttributeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.github.sparkzxl.auth.domain.service.es;
+
+import com.github.sparkzxl.auth.application.service.es.IEsOrgAttributeService;
+import com.github.sparkzxl.auth.application.service.es.IEsUserAttributeService;
+import com.github.sparkzxl.elasticsearch.service.base.BaseElasticsearchServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * description: ES组织属性 服务实现类
+ *
+ * @author zhouxinlei
+ * @date 2021-04-16 21:25:35
+ */
+@Service
+@Slf4j
+public class EsOrgAttributeServiceImpl extends BaseElasticsearchServiceImpl implements IEsOrgAttributeService {
+
+}

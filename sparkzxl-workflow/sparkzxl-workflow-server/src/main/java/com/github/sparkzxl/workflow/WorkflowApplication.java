d94241adbc4ba31dc15cf801e8f2997a5bb30fb5
zhouxinlei <zhouxinlei298@163.com>
2021-06-12 14:18:14 +0800

    111

--- sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/WorkflowApplication.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/WorkflowApplication.java
@@ -3,14 +3,11 @@ package com.github.sparkzxl.workflow;
 import com.github.sparkzxl.boot.SparkBootApplication;
 import com.github.sparkzxl.core.spring.SpringContextUtils;
 import com.github.sparkzxl.log.netty.LogWebSocketHandler;
-import com.github.sparkzxl.log.netty.NettyServer;
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * description: 工作流引擎启动类

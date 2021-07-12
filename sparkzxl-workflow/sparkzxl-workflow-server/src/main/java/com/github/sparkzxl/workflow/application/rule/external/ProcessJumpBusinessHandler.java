6f6345720ee6719ed309b06bcc79a2ba786c6f00
zhouxinlei <zhouxinlei298@163.com>
2021-07-12 22:22:54 +0800

    代码优化

--- sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/application/rule/external/ProcessJumpBusinessHandler.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/application/rule/external/ProcessJumpBusinessHandler.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
--- sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/application/rule/external/ProcessRollbackBusinessHandler.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/application/rule/external/ProcessRollbackBusinessHandler.java
@@ -1,22 +1,15 @@
 package com.github.sparkzxl.workflow.application.rule.external;
 
-import cn.hutool.core.exceptions.ExceptionUtil;
-import com.github.sparkzxl.core.support.BizExceptionAssert;
 import com.github.sparkzxl.patterns.annonation.BusinessStrategy;
 import com.github.sparkzxl.patterns.strategy.BusinessHandler;
 import com.github.sparkzxl.redisson.annotation.RedisLock;
-import com.github.sparkzxl.workflow.application.service.act.IProcessRuntimeService;
 import com.github.sparkzxl.workflow.domain.model.DriveProcess;
-import com.github.sparkzxl.workflow.domain.model.DriverData;
 import com.github.sparkzxl.workflow.domain.service.act.ActWorkApiService;
 import com.github.sparkzxl.workflow.dto.DriverResult;
 import com.github.sparkzxl.workflow.infrastructure.constant.WorkflowConstants;
 import lombok.extern.slf4j.Slf4j;
-import org.activiti.engine.runtime.ProcessInstance;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 /**
  * description: 流程驳回业务处理
@@ -28,13 +21,7 @@ import org.springframework.transaction.interceptor.TransactionAspectSupport;
 @BusinessStrategy(type = WorkflowConstants.BusinessTaskStrategy.BUSINESS_TASK_DRIVER, source = WorkflowConstants.BusinessTaskStrategy.ROLLBACK)
 public class ProcessRollbackBusinessHandler implements BusinessHandler<DriverResult, DriveProcess> {
 
-    private IProcessRuntimeService processRuntimeService;
     private ActWorkApiService actWorkApiService;
-
-    @Autowired
-    public void setProcessRuntimeService(IProcessRuntimeService processRuntimeService) {
-        this.processRuntimeService = processRuntimeService;
-    }
 
     @Autowired
     public void setActWorkApiService(ActWorkApiService actWorkApiService) {
@@ -46,32 +33,7 @@ public class ProcessRollbackBusinessHandler implements BusinessHandler<DriverRes
     @Transactional(rollbackFor = Exception.class)
     public DriverResult businessHandler(DriveProcess driveProcess) {
         log.info("流程驳回业务处理：actType:[{}],businessId:[{}]", driveProcess.getActType(), driveProcess.getBusinessId());
-        DriverResult driverResult = new DriverResult();
-        String businessId = driveProcess.getBusinessId();
-        try {
-            String userId = driveProcess.getUserId();
-            int actType = driveProcess.getActType();
-            ProcessInstance processInstance = processRuntimeService.getProcessInstanceByBusinessId(businessId);
-            if (ObjectUtils.isEmpty(processInstance)) {
-                BizExceptionAssert.businessFail("流程实例为空，请检查参数是否正确");
-            }
-            String processDefinitionKey = processInstance.getProcessDefinitionKey();
-            String processInstanceId = processInstance.getProcessInstanceId();
-            DriverData driverData = DriverData.builder()
-                    .userId(userId)
-                    .processInstanceId(processInstanceId)
-                    .processDefinitionKey(processDefinitionKey)
-                    .businessId(businessId)
-                    .comment(driveProcess.getComment())
-                    .actType(actType)
-                    .build();
-            driverResult = actWorkApiService.jumpProcess(driverData);
-        } catch (Exception e) {
-            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
-            driverResult.setErrorMsg(e.getMessage());
-            log.error("发生异常 Exception：{}", ExceptionUtil.getMessage(e));
-        }
-        return driverResult;
+        return actWorkApiService.submitProcess(driveProcess);
     }
 
 }

19fadbfb6fae131a1fa2606f1fa092f29cf5d6b5
zhouxinlei <zhouxinlei@cngongbao.com>
2021-07-18 14:01:26 +0800

    流程代码优化管理

--- sparkzxl-auth-server/src/main/java/com/github/sparkzxl/auth/application/event/InitRolePathApplicationRunner.java
+++ sparkzxl-auth-server/src/main/java/com/github/sparkzxl/auth/application/event/InitRolePathApplicationRunner.java
@@ -1,16 +1,11 @@
 package com.github.sparkzxl.auth.application.event;
 
 import com.github.sparkzxl.auth.application.service.IRoleAuthorityService;
-import com.github.sparkzxl.auth.infrastructure.client.TenantClient;
 import com.github.sparkzxl.core.context.BaseContextHolder;
-import com.github.sparkzxl.tenant.api.dto.TenantInfoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * description: 初始化角色资源加载类
@@ -22,26 +17,16 @@ import java.util.stream.Collectors;
 public class InitRolePathApplicationRunner implements CommandLineRunner, Ordered {
 
     private IRoleAuthorityService roleAuthorityService;
-    private TenantClient tenantClient;
 
     @Autowired
     public void setRoleAuthorityService(IRoleAuthorityService roleAuthorityService) {
         this.roleAuthorityService = roleAuthorityService;
     }
 
-    @Autowired
-    public void setTenantClient(TenantClient tenantClient) {
-        this.tenantClient = tenantClient;
-    }
-
     @Override
     public void run(String... args) {
-        List<TenantInfoVO> tenantInfoList = tenantClient.getTenantInfoList();
-        List<String> tenantIdList = tenantInfoList.stream().map(TenantInfoVO::getCode).collect(Collectors.toList());
-        tenantIdList.forEach(tenantId -> {
-            BaseContextHolder.setTenant("hz");
-            roleAuthorityService.refreshAuthorityList();
-        });
+        BaseContextHolder.setTenant("hz");
+        roleAuthorityService.refreshAuthorityList();
     }
 
     @Override

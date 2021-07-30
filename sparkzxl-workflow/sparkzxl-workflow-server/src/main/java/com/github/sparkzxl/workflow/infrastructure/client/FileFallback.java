7558f016d779d929b16dcb75c620f7df1b01e101
zhouxinlei <zhouxinlei@cngongbao.com>
2021-07-30 17:53:42 +0800

    异常传递信息

--- sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/infrastructure/client/FileFallback.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/infrastructure/client/FileFallback.java
@@ -2,6 +2,7 @@ package com.github.sparkzxl.workflow.infrastructure.client;
 
 import com.github.sparkzxl.core.support.ExceptionAssert;
 import com.github.sparkzxl.feign.exception.RemoteCallException;
+import com.github.sparkzxl.file.dto.FileDTO;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,9 +12,20 @@ import org.springframework.stereotype.Component;
 public class FileFallback implements FallbackFactory<FileClient> {
     @Override
     public FileClient create(Throwable throwable) {
-        if (throwable instanceof RemoteCallException) {
-            ExceptionAssert.failure(throwable.getMessage());
-        }
-        return null;
+        return new FileClient() {
+            @Override
+            public FileDTO getPdf(FileDTO fileDTO) {
+                // 部分接口需要捕获异常
+                if (throwable instanceof RemoteCallException) {
+                    ExceptionAssert.failure(throwable.getMessage());
+                }
+                return null;
+            }
+
+            @Override
+            public FileDTO getHtml(FileDTO fileDTO) {
+                return null;
+            }
+        };
     }
 }

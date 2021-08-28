98b70b06a1b90aa4104d2086e4154a35d4e98103
zhouxinlei <zhouxinlei298@163.com>
2021-08-28 21:23:56 +0800

    代码优化

--- sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
@@ -2,6 +2,7 @@ package com.github.sparkzxl.workflow.domain.repository.mongodb;
 
 import cn.hutool.json.JSONUtil;
 import com.github.sparkzxl.core.context.AppContextHolder;
+import com.github.sparkzxl.core.utils.DateUtils;
 import com.github.sparkzxl.workflow.WorkflowApplication;
 import com.github.sparkzxl.workflow.infrastructure.entity.mongodb.OnlFormTemplate;
 import org.junit.jupiter.api.Test;
@@ -21,8 +22,8 @@ public class OnlFormTemplateRepositoryTest {
     void test() {
         AppContextHolder.setUserId("admin");
         OnlFormTemplate onlFormTemplate = new OnlFormTemplate();
-        onlFormTemplate.setTemplateCode("test_template");
-        onlFormTemplate.setTemplateName("测试模板");
+        onlFormTemplate.setTemplateCode("test_template1");
+        onlFormTemplate.setTemplateName("测试模板1");
         onlFormTemplate.setTemplateJson("{\"diffPayment\": 1, \"contractType\": 1, \"paymentScale\": \"http://172.16.200.202:9000/gongbaojin-user/payment_scale/20210824192910XF8GUs7D.jpg\", \"constructionLicense\": \"http://172.16.200.202:9000/gongbaojin-user/construction_license/202108241929183JpqRjQP.png\", \"constructionContract\": \"http://172.16.200.202:9000/gongbaojin-user/construction_contract/20210824192911uDrlcydn.jpg\"}");
         templateRepository.insert(onlFormTemplate);
     }
@@ -34,6 +35,7 @@ public class OnlFormTemplateRepositoryTest {
         onlFormTemplate.setTemplateCode("test_template");
         Example<OnlFormTemplate> example = Example.of(onlFormTemplate);
         OnlFormTemplate template = templateRepository.findOne(example).orElse(null);
+        System.out.println(DateUtils.formatLocalDateTime(template.getCreateTime()));
         System.out.println(JSONUtil.toJsonPrettyStr(template));
     }
 

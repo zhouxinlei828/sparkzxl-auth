a8e91613fd7fca1b5547f4e5526ed472741b9dc8
zhouxinlei <zhouxinlei298@163.com>
2021-09-02 21:52:37 +0800

    代码优化

--- sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
@@ -2,7 +2,6 @@ package com.github.sparkzxl.workflow.domain.repository.mongodb;
 
 import cn.hutool.json.JSONUtil;
 import com.github.sparkzxl.core.context.AppContextHolder;
-import com.github.sparkzxl.core.support.BizException;
 import com.github.sparkzxl.core.utils.DateUtils;
 import com.github.sparkzxl.mongodb.dynamic.DynamicDatabaseContextHolder;
 import com.github.sparkzxl.workflow.WorkflowApplication;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = {WorkflowApplication.class}, properties = {"spring.profiles.active=dev"})
 public class OnlFormTemplateRepositoryTest {
@@ -20,7 +18,6 @@ public class OnlFormTemplateRepositoryTest {
     private OnlFormTemplateRepository templateRepository;
 
     @Test
-    @Transactional(value = "mongoTransactionManager", rollbackFor = Exception.class)
     void test() {
         AppContextHolder.setUserId("admin");
         DynamicDatabaseContextHolder.push("hz");
@@ -29,12 +26,11 @@ public class OnlFormTemplateRepositoryTest {
         onlFormTemplate.setTemplateName("测试模板");
         onlFormTemplate.setTemplateJson("{\"column\":[{\"type\":\"title\",\"span\":24,\"display\":true,\"styles\":{\"fontSize\":\"18px\",\"color\":\"#000\"},\"labelWidth\":\"50%\",\"value\":\"请假条\",\"prop\":\"1630205651504_44581\",\"label\":\"\"},{\"type\":\"input\",\"label\":\"请假标题\",\"span\":12,\"display\":true,\"prop\":\"1630205696341_50071\",\"labelTip\":\"请输入请假事由\",\"labelTipPlacement\":\"bottom\"},{\"type\":\"daterange\",\"label\":\"请假时间\",\"span\":12,\"display\":true,\"format\":\"yyyy-MM-dd\",\"valueFormat\":\"yyyy-MM-dd\",\"prop\":\"1630205813595_74698\"},{\"type\":\"ueditor\",\"component\":\"avue-ueditor\",\"label\":\"详细事由\",\"span\":24,\"display\":true,\"options\":{\"action\":\"\",\"oss\":\"\",\"props\":{},\"ali\":{},\"qiniu\":{}},\"prop\":\"1630205854232_45542\"}],\"labelPosition\":\"left\",\"labelSuffix\":\"：\",\"labelWidth\":120,\"gutter\":0,\"menuBtn\":true,\"submitBtn\":true,\"submitText\":\"提交\",\"emptyBtn\":true,\"emptyText\":\"清空\",\"menuPosition\":\"center\",\"tabs\":false}");
         templateRepository.insert(onlFormTemplate);
-        throw new BizException(500, "回滚代码");
     }
 
     @Test
     void testFindTemplate() {
-        DynamicDatabaseContextHolder.push("hz");
+        //DynamicDatabaseContextHolder.push("hz");
         OnlFormTemplate onlFormTemplate = new OnlFormTemplate();
         onlFormTemplate.setTemplateCode("test_template1");
         Example<OnlFormTemplate> example = Example.of(onlFormTemplate);

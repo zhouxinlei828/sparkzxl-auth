9f870ac646e88c5d35b13e08d8611f11a73cbc81
zhouxinlei <zhouxinlei@cngongbao.com>
2021-09-03 16:14:27 +0800

    MongoDB代码优化

--- sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/test/java/com/github/sparkzxl/workflow/domain/repository/mongodb/OnlFormTemplateRepositoryTest.java
@@ -20,11 +20,15 @@ public class OnlFormTemplateRepositoryTest {
     @Test
     void test() {
         AppContextHolder.setUserId("admin");
-        DynamicDatabaseContextHolder.push("hz");
+        // 动态数据源切换
         OnlFormTemplate onlFormTemplate = new OnlFormTemplate();
         onlFormTemplate.setTemplateCode("test_template");
         onlFormTemplate.setTemplateName("测试模板");
         onlFormTemplate.setTemplateJson("{\"column\":[{\"type\":\"title\",\"span\":24,\"display\":true,\"styles\":{\"fontSize\":\"18px\",\"color\":\"#000\"},\"labelWidth\":\"50%\",\"value\":\"请假条\",\"prop\":\"1630205651504_44581\",\"label\":\"\"},{\"type\":\"input\",\"label\":\"请假标题\",\"span\":12,\"display\":true,\"prop\":\"1630205696341_50071\",\"labelTip\":\"请输入请假事由\",\"labelTipPlacement\":\"bottom\"},{\"type\":\"daterange\",\"label\":\"请假时间\",\"span\":12,\"display\":true,\"format\":\"yyyy-MM-dd\",\"valueFormat\":\"yyyy-MM-dd\",\"prop\":\"1630205813595_74698\"},{\"type\":\"ueditor\",\"component\":\"avue-ueditor\",\"label\":\"详细事由\",\"span\":24,\"display\":true,\"options\":{\"action\":\"\",\"oss\":\"\",\"props\":{},\"ali\":{},\"qiniu\":{}},\"prop\":\"1630205854232_45542\"}],\"labelPosition\":\"left\",\"labelSuffix\":\"：\",\"labelWidth\":120,\"gutter\":0,\"menuBtn\":true,\"submitBtn\":true,\"submitText\":\"提交\",\"emptyBtn\":true,\"emptyText\":\"清空\",\"menuPosition\":\"center\",\"tabs\":false}");
+        templateRepository.insert(onlFormTemplate);
+        DynamicDatabaseContextHolder.push("demo0");
+        templateRepository.insert(onlFormTemplate);
+        DynamicDatabaseContextHolder.push("demo1");
         templateRepository.insert(onlFormTemplate);
     }
 

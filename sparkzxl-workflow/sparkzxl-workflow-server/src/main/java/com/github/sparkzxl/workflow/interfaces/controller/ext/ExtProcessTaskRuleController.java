7b206151ba6ba293d0b0bbef369c289a7fd949b5
zhouxinlei <zhouxinlei@cngongbao.com>
2021-09-22 17:28:53 +0800

    代码优化

--- sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/interfaces/controller/ext/ExtProcessTaskRuleController.java
+++ sparkzxl-workflow/sparkzxl-workflow-server/src/main/java/com/github/sparkzxl/workflow/interfaces/controller/ext/ExtProcessTaskRuleController.java
@@ -1,6 +1,7 @@
 package com.github.sparkzxl.workflow.interfaces.controller.ext;
 
 import com.github.sparkzxl.annotation.result.ResponseResult;
+import com.github.sparkzxl.log.annotation.OptLogRecord;
 import com.github.sparkzxl.log.annotation.WebLog;
 import com.github.sparkzxl.workflow.application.service.ext.IExtProcessTaskRuleService;
 import com.github.sparkzxl.workflow.infrastructure.entity.ExtProcessTaskRule;
@@ -43,6 +44,7 @@ public class ExtProcessTaskRuleController {
             @ApiImplicitParam(name = "procDefKey", value = "流程定义key", required = true),
             @ApiImplicitParam(name = "taskDefKey", value = "任务定义key", required = true)
     })
+    @OptLogRecord(bizNo = "#procDefKey", category = "查询流程跳转规则", detail = "查询流程{}，任务{}跳转规则", condition = "#procDefKey,#taskDefKey")
     public List<ExtProcessTaskRule> getProcessTaskRule(@RequestParam("procDefKey") String procDefKey,
                                                        @RequestParam("taskDefKey") String taskDefKey) {
         return processTaskRuleService.getProcessTaskRule(procDefKey, taskDefKey);

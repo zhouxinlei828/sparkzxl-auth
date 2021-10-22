e5b2bd5c5a7951eeba79648b02158d968940686a
zhouxinlei <zhouxinlei@cngongbao.com>
2021-10-22 22:30:25 +0800

    代码优化

--- sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/RouteLoadBalancerClientConfig.java
+++ sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/RouteLoadBalancerClientConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * description: 灰度负载模式自动装配
+ * description: 网关路由负载模式自动装配
  *
  * @author zhouxinlei
  * @date 2021-10-22 22:00:19

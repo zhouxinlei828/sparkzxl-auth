d6312f5b8ccebea18b48aa44f762c407ae228160
zhouxinlei <zhouxinlei298@163.com>
2022-06-05 14:52:49 +0800

    默认feign client config处理

--- sparkzxl-gateway-server/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/CircuitBreakerAutoConfig.java
+++ sparkzxl-gateway-server/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/CircuitBreakerAutoConfig.java
@@ -1,14 +1,9 @@
 package com.github.sparkzxl.gateway.infrastructure.config;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 
 /**
  * description: CircuitBreaker auto  Config
@@ -21,11 +16,13 @@ public class CircuitBreakerAutoConfig {
 
 
     @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> customizerReactiveCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry, TimeLimiterRegistry timeLimiterRegistry) {
-        return (reactiveCircuitBreakerFactory) -> reactiveCircuitBreakerFactory.configure(
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> customizerReactiveCircuitBreakerFactory() {
+        return reactiveCircuitBreakerFactory -> reactiveCircuitBreakerFactory.configure(
                 builder -> builder
-                        .timeLimiterConfig(timeLimiterRegistry.getConfiguration("backendB").orElse(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(300)).build()))
-                        .circuitBreakerConfig(circuitBreakerRegistry.getConfiguration("backendB").orElse(circuitBreakerRegistry.getDefaultConfig())),
+                        .timeLimiterConfig(reactiveCircuitBreakerFactory.getTimeLimiterRegistry().getConfiguration("backendB")
+                                .orElse(reactiveCircuitBreakerFactory.getTimeLimiterRegistry().getDefaultConfig()))
+                        .circuitBreakerConfig(reactiveCircuitBreakerFactory.getCircuitBreakerRegistry().getConfiguration("backendB")
+                                .orElse(reactiveCircuitBreakerFactory.getCircuitBreakerRegistry().getDefaultConfig())),
                 "backendB");
     }
 }
